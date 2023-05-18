297e3349feafb29a435b04b84da0acfd55240f28
Evilmoose <ronnielopez@gmail.com>
2023-05-18 10:34:24 -0700

    Page Playwright automation test.

--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -1,11 +1,21 @@
 import {test} from '@playwright/test';
 
-// test(string, testfunction)
+// test(string, testfunction) 
+// This test os for reference
 test('First PLaywright test', async ({browser}) => {
     // playwright code goes here
 
     // chrome - plugins/cookies
     const context = await browser.newContext();
+
+    const page = await context.newPage();
     
+    await page.goto('http://rahulshettyacademy.com/loginpagePractise/');
+})
+
+test('Page Playwright test', async ({page}) => {
     
+    await page.goto('http://rahulshettyacademy.com/loginpagePractise/');
+    // continue with automation
+
 } )
