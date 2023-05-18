fcbe7a2700e74700b70a4c7e7c661ce26e9c9380
Evilmoose <ronnielopez@gmail.com>
2023-05-18 10:23:42 -0700

    brower.newContext()

--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -1,6 +1,11 @@
-const {test} = require('@playwright/test');
+import {test} from '@playwright/test';
 
 // test(string, testfunction)
-test('First PLaywright test', () => {
+test('First PLaywright test', async ({browser}) => {
     // playwright code goes here
+
+    // chrome - plugins/cookies
+    const context = await browser.newContext();
+    
+    
 } )
