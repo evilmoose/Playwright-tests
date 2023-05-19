c09a59aec9817a35b9b7971edc142176be002093
Evilmoose <ronnielopez@gmail.com>
2023-05-19 11:50:55 -0700

    using fill

--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -5,13 +5,24 @@ import {expect, test} from '@playwright/test';
 test('Browser Context Playwright test', async ({browser}) => {
     // playwright code goes here
 
+    
     // chrome - plugins/cookies
     const context = await browser.newContext();
     const page = await context.newPage();
+    const userName = page.locator('#username');
+    const signIn = page.locator("#signInBtn");
     await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
     console.log(await page.title());
-
-    await page.locator('#username').type("rahulshetty");
+    // css
+    await userName.type("rahulshetty");
     await page.locator("[type='password']").type("learning");
-    await page.locator("#signInBtn").click();
+    await signIn.click();
+    console.log(await page.locator("[style*='block']").textContent());
+    await expect(page.locator("[style*='block']")).toContainText('Incorrect');
+    // type - fill
+    await userName.fill("");
+    await userName.fill("rahulshettyacademy");
+    await signIn.click();
+    console.log(await page.locator(".card-body a").first().textContent());
+    console.log(await page.locator(".card-body a").nth(1).textContent());
 })
