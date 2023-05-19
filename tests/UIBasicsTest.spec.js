1167f1c4a413a669ccb6f4648d2889d313c86afa
Evilmoose <ronnielopez@gmail.com>
2023-05-18 20:22:11 -0700

    Form testing

--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -1,19 +1,17 @@
-import {test} from '@playwright/test';
+import {expect, test} from '@playwright/test';
 
 // test(string, testfunction) 
 // This test os for reference
-test('Browser Context PLaywright test', async ({browser}) => {
+test('Browser Context Playwright test', async ({browser}) => {
     // playwright code goes here
 
     // chrome - plugins/cookies
     const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+    console.log(await page.title());
+
+    await page.locator('#username').type("rahulshetty");
+    await page.locator("[type='password']").type("learning");
+    await page.locator("#signInBtn").click();
 })
-
-test('Page Playwright test', async ({page}) => {
-    
-    await page.goto('https://google.com');
-    // continue with automation
-
-} )
