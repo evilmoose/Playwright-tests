ecf7d4774e94eec4a09032d55dd469f3b75e3f9c
Evilmoose <ronnielopez@gmail.com>
2023-05-18 19:11:23 -0700

    Cleaned up some code and switch to ESModules

--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -2,20 +2,18 @@ import {test} from '@playwright/test';
 
 // test(string, testfunction) 
 // This test os for reference
-test('First PLaywright test', async ({browser}) => {
+test('Browser Context PLaywright test', async ({browser}) => {
     // playwright code goes here
 
     // chrome - plugins/cookies
     const context = await browser.newContext();
-
     const page = await context.newPage();
-    
-    await page.goto('http://rahulshettyacademy.com/loginpagePractise/');
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
 })
 
 test('Page Playwright test', async ({page}) => {
     
-    await page.goto('http://rahulshettyacademy.com/loginpagePractise/');
+    await page.goto('https://google.com');
     // continue with automation
 
 } )
